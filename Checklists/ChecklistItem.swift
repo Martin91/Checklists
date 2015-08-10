d53f27210bf739402b43ab4c82c754e9c8551084
Martin Hong <hongzeqin@gmail.com>
2015-08-10 08:29:44 +0800

    add checklist item model to represent table cell data and its status

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Martin Hong on 8/10/15.
+//  Copyright (c) 2015 Martin Hong. All rights reserved.
+//
+
+import Foundation
+
+class CheckListItem {
+    var text = ""
+    var checked = false
+}
